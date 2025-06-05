246b2fb3136c5af20622cfe9605910006f5ee740
Murilo Pereira Coelho <murilo.coelho@mca.ufcspa.edu.br>
2025-06-05 11:32:31 -0300

    prime number challenge

---
+++ challenges/primeNumber/primeNumber.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var input int
+
+	fmt.Println("Digite um número inteiro e descubra se ele é primo ou não:")
+	fmt.Scanln(&input)
+
+	//o número é primo se ele só for divisível por 1 ou por ele mesmo
+
+	for i := 2;i <= input; i++ {
+		if (input % i == 0 && i != input) {
+			fmt.Println("Não é primo")
+			break
+		} 
+		
+		if (i == input) {
+			fmt.Println("É primo")
+		}
+	} 
+}
