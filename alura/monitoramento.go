5b722f73b0b6f730391c0e351afed7fa085f40a4
Murilo Pereira Coelho <murilopc14@hotmail.com>
2025-06-27 02:54:11 -0300

    set time functions

--- alura/monitoramento.go
+++ alura/monitoramento.go
@@ -3,28 +3,42 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
+	iniciarMonitoramento()
+}
 
-	for {
-		res, err := iniciarMonitoramento()
+const monitoramento = 3
+const delay = 5
 
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	sites := []string{
+		"https://ufcspa.edu.br/",
+		"https://www.alura.com.br/",
+		"https://qi.edu.br/faqi/",
+		"https://httpstat.us/random/200,201,400-404,500-504"}
+	
+	for i := 0; i < monitoramento; i++ {
+		for i, site := range sites {
+			fmt.Println("Estou passando na posição", i, "do meu slice e essa posição tem o site:", site)
+			testaSite(site)
+		}
+
+		time.Sleep(delay * time.Second)
+		fmt.Println("")
+	}
+	fmt.Println("")
+}
+
+func testaSite(url string) {
+		res, err := http.Get(url)
 
 		if res.StatusCode == 200 {
 			fmt.Println("Response: ", res.Status)
 		} else {
 			fmt.Println("Request Error: ", err, res.Status)
 		}
-
-	}
-}
-
-func iniciarMonitoramento() (*http.Response, error) {
-	fmt.Println("Monitorando...")
-	siteUrl := "https://httpstat.us/random/200,201,400-404,500-504"
-	// siteUrl = "https://www.aludsra.com.br/"
-	res, err := http.Get(siteUrl)
-
-	return res, err
 }
