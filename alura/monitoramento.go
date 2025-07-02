06a13130edc264a0e85a4672887ab8951b4cbf68
Murilo Pereira Coelho <murilopc14@hotmail.com>
2025-07-02 01:07:09 -0300

    error handling

--- alura/monitoramento.go
+++ alura/monitoramento.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 )
 
 func main() {
 	iniciarMonitoramento()
+	lerSitesDoArquivo()
 }
 
 const monitoramento = 3
@@ -19,8 +21,8 @@ func iniciarMonitoramento() {
 		"https://ufcspa.edu.br/",
 		"https://www.alura.com.br/",
 		"https://qi.edu.br/faqi/",
-		"https://httpstat.us/random/200,201,400-404,500-504"}
-	
+	}
+
 	for i := 0; i < monitoramento; i++ {
 		for i, site := range sites {
 			fmt.Println("Estou passando na posição", i, "do meu slice e essa posição tem o site:", site)
@@ -34,11 +36,24 @@ func iniciarMonitoramento() {
 }
 
 func testaSite(url string) {
-		res, err := http.Get(url)
+	res, err := http.Get(url)
 
-		if res.StatusCode == 200 {
-			fmt.Println("Response: ", res.Status)
-		} else {
-			fmt.Println("Request Error: ", err, res.Status)
-		}
+	if res.StatusCode == 200 {
+		fmt.Println("Response: ", res.Status)
+	} else {
+		fmt.Println("Request Error: ", err)
+	}
+}
+
+func lerSitesDoArquivo() []string {
+	var sites []string
+
+	arquivo, err := os.Open("alura/sites.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro: ", err)
+	}
+
+	fmt.Println(arquivo)
+	return sites
 }
