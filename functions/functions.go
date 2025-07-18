20eb3807faf09143762c4b56a1f6784a7b0e5be4
Murilo Pereira Coelho <murilopc14@hotmail.com>
2025-07-18 09:55:42 -0300

    Funções variádicas

--- functions/functions.go
+++ functions/functions.go
@@ -8,6 +8,9 @@ import (
 func main() {
 	x1,x2 := bhaskara(1, -5, 6)
 	fmt.Printf("x1= %.2f | x2= %.2f", x1, x2)
+	
+	total := sum(1, 4, 2.4, 6, 2, 7.6, 11, 43.3, 9)
+	fmt.Printf("Total: %.2f", total)
 }
 
 //Go permite criar funções com múltiplos retornos
@@ -24,4 +27,14 @@ func bhaskara(a float64, b float64, c float64) (float64, float64) {
 func delta(a float64, b float64, c float64) float64 {
 	delta := math.Pow(b, 2) - 4*a*c
 	return delta
+}
+
+//Função Variádica, permite estabelecer uma função com número de parâmetros indefinido
+func sum(numbers ...float64) float64 {
+	result := 0.0
+	for _, number := range numbers {
+		result += number
+	}
+
+	return result
 }
