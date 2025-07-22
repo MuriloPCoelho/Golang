574ea4750b949643518d9876c2b440e8d6931f11
Murilo Pereira Coelho <murilopc14@hotmail.com>
2025-07-21 21:40:46 -0300

    Separa a estrutura contaCorrente

---
+++ POO/contas/contaCorrente.go
@@ -0,0 +1,38 @@
+package contas
+
+import "fmt"
+
+type ContaCorrente struct {
+	Titular       string
+	NumeroAgencia int
+	NumeroConta   int
+	Saldo         float64
+}
+
+func (c *ContaCorrente) Sacar(valorDoSaque float64) {
+	if valorDoSaque <= c.Saldo {
+		c.Saldo -= valorDoSaque
+		fmt.Printf("Saque realizado com sucesso. O Saldo atual é de: %.2f\n", c.Saldo)
+	} else {
+		fmt.Println("Saldo indisponível")
+	}
+}
+
+func (c *ContaCorrente) Depositar(valorDeposito float64) {
+	if valorDeposito > 0 {
+		c.Saldo += valorDeposito
+		fmt.Printf("Deposito realizado com sucesso. O Saldo atual é de: %.2f\n", c.Saldo)
+	} else {
+		fmt.Println("Valor de deposito inválido")
+	}
+}
+
+func (c *ContaCorrente) Transferir(valorTransferencia float64, contaDestino *ContaCorrente) {
+	if c.Saldo > valorTransferencia && valorTransferencia > 0 {
+		c.Saldo -= valorTransferencia
+		contaDestino.Depositar(valorTransferencia)
+		fmt.Println("Transferência realizada com sucesso")
+	} else {
+		fmt.Println("Não foi possível realizar a transferência")
+	}
+}
--- POO/main.go
+++ POO/main.go
@@ -1,52 +1,13 @@
 package main
 
 import (
+	"example/hello/POO/contas"
 	"fmt"
 )
 
-type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroConta   int
-	saldo         float64
-}
-
-func (c *ContaCorrente) Sacar(valorDoSaque float64) {
-	if valorDoSaque <= c.saldo {
-		c.saldo -= valorDoSaque
-		fmt.Printf("Saque realizado com sucesso. O saldo atual é de: %.2f\n", c.saldo)
-	} else {
-		fmt.Println("Saldo indisponível")
-	}
-}
-
-func (c *ContaCorrente) Depositar(valorDeposito float64) {
-	if valorDeposito > 0 {
-		c.saldo += valorDeposito
-		fmt.Printf("Deposito realizado com sucesso. O saldo atual é de: %.2f\n", c.saldo)
-	} else {
-		fmt.Println("Valor de deposito inválido")
-	}
-}
-
-func (c *ContaCorrente) Transferir(valorTransferencia float64, contaDestino *ContaCorrente) {
-	if c.saldo > valorTransferencia && valorTransferencia > 0 {
-		c.saldo -= valorTransferencia
-		contaDestino.saldo += valorTransferencia
-		fmt.Println("Transferência realizada com sucesso")
-	} else {
-		fmt.Println("Não foi possível realizar a transferência")
-	}
-}
-
 func main() {
-	cliente1 := ContaCorrente{"Murilo", 531, 642332, 245.32}
-	cliente2 := ContaCorrente{"Murilo", 531, 642332, 245.32}
-
-	// var cliente2 *ContaCorrente
-	// cliente2 := new(ContaCorrente)
-	// cliente2.titular = "Cris"
-	// cliente2.saldo = 500
+	cliente1 := contas.ContaCorrente{Titular: "Murilo", NumeroAgencia: 531, NumeroConta: 642332, Saldo: 245.32}
+	cliente2 := contas.ContaCorrente{Titular: "Murilo", NumeroAgencia: 531, NumeroConta: 642332, Saldo: 245.32}
 
 	fmt.Println(&cliente1 == &cliente2)
 	fmt.Println(cliente1 == cliente2)
