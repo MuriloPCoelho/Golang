412a83f266851304df6937ebae8bc4dd58602940
Murilo Pereira Coelho <murilopc14@hotmail.com>
2025-07-22 23:59:15 -0300

    feat: Adiciona struct de contaPoupanca

---
+++ POO/contas/contaPoupanca.go
@@ -0,0 +1,30 @@
+package contas
+
+import (
+	"example/hello/POO/clientes"
+	"fmt"
+)
+
+type ContaPoupanca struct {
+	Titular                              clientes.Titular
+	NumeroAgencia, NumeroConta, Operacao int
+	saldo                                float64
+}
+
+func (c *ContaPoupanca) Sacar(valorDoSaque float64) {
+	if valorDoSaque <= c.saldo {
+		c.saldo -= valorDoSaque
+		fmt.Printf("Saque realizado com sucesso. O saldo atual é de: %.2f\n", c.saldo)
+	} else {
+		fmt.Println("saldo indisponível")
+	}
+}
+
+func (c *ContaPoupanca) Depositar(valorDeposito float64) {
+	if valorDeposito > 0 {
+		c.saldo += valorDeposito
+		fmt.Printf("Deposito realizado com sucesso. O saldo atual é de: %.2f\n", c.saldo)
+	} else {
+		fmt.Println("Valor de deposito inválido")
+	}
+}
--- POO/main.go
+++ POO/main.go
@@ -9,9 +9,12 @@ import (
 func main() {
 	cliente1 := contas.ContaCorrente{Titular: clientes.Titular{Nome: "Murilo", CPF: "1231231112", Profissao: "Analista Juridico"}, NumeroAgencia: 531, NumeroConta: 642332}
 	cliente1.Depositar(245.32)
+
 	clienteJairo := clientes.Titular{Nome: "Jairo", CPF: "1231231112", Profissao: "Politico"}
 	contaJairo := contas.ContaCorrente{Titular: clienteJairo, NumeroAgencia: 531, NumeroConta: 642332}
 	contaJairo.Depositar(245.32)
+
+	contaPoupancaJaime := contas.ContaPoupanca{Titular: clientes.Titular{Nome: "Jaime", CPF: "1231231112", Profissao: "Bombeiro"}, NumeroAgencia: 156, NumeroConta: 874672, Operacao: 1}
 
 	fmt.Println(&cliente1 == &contaJairo)
 	fmt.Println(cliente1 == contaJairo)
@@ -21,4 +24,5 @@ func main() {
 	cliente1.Transferir(100, &contaJairo)
 	fmt.Println(cliente1)
 	fmt.Println(contaJairo)
+	fmt.Println(contaPoupancaJaime)
 }
