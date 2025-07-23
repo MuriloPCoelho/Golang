6791255bb02eb1a9135345cb8fc29e601f8ec985
Murilo Pereira Coelho <murilopc14@hotmail.com>
2025-07-22 23:29:22 -0300

    fix: Ajusta clientes para utilizar struct Titular

--- POO/main.go
+++ POO/main.go
@@ -1,20 +1,22 @@
 package main
 
 import (
+	"example/hello/POO/clientes"
 	"example/hello/POO/contas"
 	"fmt"
 )
 
 func main() {
-	cliente1 := contas.ContaCorrente{Titular: "Murilo", NumeroAgencia: 531, NumeroConta: 642332, Saldo: 245.32}
-	cliente2 := contas.ContaCorrente{Titular: "Murilo", NumeroAgencia: 531, NumeroConta: 642332, Saldo: 245.32}
+	cliente1 := contas.ContaCorrente{Titular: clientes.Titular{Nome: "Murilo", CPF: "1231231112", Profissao: "Analista Juridico"}, NumeroAgencia: 531, NumeroConta: 642332, Saldo: 245.32}
+	clienteJairo := clientes.Titular{Nome: "Jairo", CPF: "1231231112", Profissao: "Politico"}
+	contaJairo := contas.ContaCorrente{Titular: clienteJairo, NumeroAgencia: 531, NumeroConta: 642332, Saldo: 245.32}
 
-	fmt.Println(&cliente1 == &cliente2)
-	fmt.Println(cliente1 == cliente2)
+	fmt.Println(&cliente1 == &contaJairo)
+	fmt.Println(cliente1 == contaJairo)
 	cliente1.Sacar(100)
 	cliente1.Depositar(-500)
 
-	cliente1.Transferir(100, &cliente2)
+	cliente1.Transferir(100, &contaJairo)
 	fmt.Println(cliente1)
-	fmt.Println(cliente2)
+	fmt.Println(contaJairo)
 }
